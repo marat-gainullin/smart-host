d9e19ebe732e12b027151645cf8100e34e8cc526
marat-gainullin <marat.gainullin.iv@gmail.com>
2020-03-10 22:20:07 +0100

    Base implementation of planning added

--- src/main/java/com/smarthost/trial/planning/Planner.java
+++ src/main/java/com/smarthost/trial/planning/Planner.java
@@ -7,7 +7,21 @@ import java.util.List;
 public class Planner {
 
     public static Occupation optimal(int availablePremiumRooms, int availableEconomyRooms, List<Integer> offers) {
-        return new Occupation(availablePremiumRooms, availableEconomyRooms);
+        var economyGuests = 0;
+        var premiumGuests = 0;
+        for (var offer : offers) {
+            if (offer < 100) {
+                economyGuests++;
+            } else {
+                premiumGuests++;
+            }
+        }
+        var plannedEconomyRooms = Math.min(availableEconomyRooms, economyGuests);
+        var plannedPremiumRooms = Math.min(availablePremiumRooms, premiumGuests);
+        var restPremiumRooms = availablePremiumRooms - plannedPremiumRooms;
+        var restEconomyGuests = economyGuests - plannedEconomyRooms;
+        var upgradedPremiumRooms = Math.min(restPremiumRooms, restEconomyGuests);
+        return new Occupation(plannedEconomyRooms, plannedPremiumRooms + upgradedPremiumRooms);
     }
 
 }
